fb55024f7a0452d69368c0c3de9996433ec5839b
MINHAJUL ABEDIN <mabedinfahim@gmail.com>
2022-08-01 20:34:35 +0600

    navbar responsive done

--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -1,22 +1,33 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 import logo from '../../images/logo.png'
 
 const Header = () => {
     return (
         <div>
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                <Navbar.Brand href="#home"><img className='w-50' src={logo} alt="" srcset="" /></Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/features">Features</Nav.Link>
-                    <Nav.Link href="/pricing">Pricing</Nav.Link>
-                </Nav>
-                </Container>
-            </Navbar>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#"> <img className="w-50" src={logo} alt=""/> </a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">Features</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+            </nav>
         </div>
     );
 };
