78689c064628d0a63688bb83044bac6f171a73cf
MINHAJUL ABEDIN <mabedinfahim@gmail.com>
2022-08-04 21:09:25 +0600

    serviceDetails done

--- src/pages/Home/ServiceDetail/ServiceDetail.js
+++ src/pages/Home/ServiceDetail/ServiceDetail.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import  Card  from 'react-bootstrap/Card';
+import { useParams } from 'react-router-dom';
 
 const ServiceDetail = () => {
+    const {id}=useParams()
+    const [service,setService]=useState([])
+    useEffect(()=>{
+        fetch("/services.json")
+        .then(res=>res.json())
+        .then(data=>{
+            setService(data)
+        })
+    },[])
+
+    const serviceDetails=service?.find(data=>data.id ==id)
+    console.log(serviceDetails)
+    
+    
+   
     return (
-        <div>
-            <h1>This is service page</h1>
+        <div className="d-grid col-5 mx-auto">
+            <h1 className='py-5 text-center'>This is a service page</h1>
+            <Card>
+                <Card.Img variant="top" src={serviceDetails?.img} />
+                <Card.Body>
+                <Card.Title className="text-center">{serviceDetails?.name}</Card.Title>
+                <Card.Text>
+                {serviceDetails?.description}
+                </Card.Text>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
