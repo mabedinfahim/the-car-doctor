e988e24b94aa12d31077b2edbe6b75b31eb7ec5a
MINHAJUL ABEDIN <mabedinfahim@gmail.com>
2022-09-05 22:02:44 +0600

    some change

--- src/RequireAuth/RequireAuth.js
+++ src/RequireAuth/RequireAuth.js
@@ -4,8 +4,12 @@ import { Navigate, useLocation } from 'react-router-dom';
 import auth from '../firebase.init';
 
 const RequireAuth = ({ children }) => {
-    let [user] = useAuthState(auth);
+    let [user,loading] = useAuthState(auth);
     let location = useLocation();
+
+    if(loading) {
+        return <h1 className="text-center my-5">Loading...</h1>
+    }
 
     if(!user){
        return <Navigate to="/sign-in" state={{from:location}} replace/>
@@ -14,6 +18,7 @@ const RequireAuth = ({ children }) => {
     if(!user.emailVerified){
         return <Navigate to="/verifyemail" state={{from:location}} replace/>
     }
+
     return children; 
 };
 
